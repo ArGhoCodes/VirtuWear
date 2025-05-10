d7f19c76b632f708614acbef61f2af3c8ef59d5a
Arpan Ghosh <114988864+ArGhoCodes@users.noreply.github.com>
2025-05-11 00:52:54 +0530

    feat: implement dynamic resizing of clothing images based on user pose

---
+++ resize_image.py
@@ -0,0 +1,18 @@
+import cv2
+
+def scaleImg(img, pose):
+    shoulder = pose.shoulder
+    waist = pose.waist
+    
+    width = abs(shoulder[1][0] - shoulder[0][0]) + 80
+    height = abs(shoulder[1][1] - waist[1][1]) + 40
+    
+    # print(height)
+    # print(width)
+
+    if(height != 0 and width != 0 and height >= width) :
+        img = cv2.resize(img, (width,height))
+
+    return img
+
+
